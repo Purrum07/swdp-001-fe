eec94d7991954d375722d626066340df6c3caedd
unknown <marioalex20007@hotmail.com>
2022-11-26 21:45:19 -0600

    SVMM-01 Checkboxes updated

--- swdp-fe/src/components/FormaPlantilla/index.js
+++ swdp-fe/src/components/FormaPlantilla/index.js
@@ -9,8 +9,9 @@ import { useNavigate } from "react-router-dom";
 
 function TempleteForm() {
 
+    
     const [name, setName] = React.useState('');
-    const [description, setDescription] = React.useState('');
+    const [description = false, setDescription] = React.useState('');
     const [barCode, setBarCode] = React.useState('');
     const [date, setDate] = React.useState('');
     const [client, setClient] = React.useState('');
@@ -19,33 +20,36 @@ function TempleteForm() {
     const [heigth, setHeigth] = React.useState('');
     const [width, setWidth] = React.useState('');
 
+    function firstSet(){
+        setDescription(true)
+    }
     function handleChangeName(e) {
         e.preventDefault()
         setName(e.target.value)
     }
     function handleChangeDescription(e) {
         e.preventDefault()
-        setDescription(e.target.value)
+        setDescription(e.target.checked)
     }
     function handleChangeBarCode(e) {
         e.preventDefault()
-        setBarCode(e.target.value)
+        setBarCode(e.target.checked)
     }
     function handleChangeDate(e) {
         e.preventDefault()
-        setDate(e.target.value)
+        setDate(e.target.checked)
     }
     function handleChangeClient(e) {
         e.preventDefault()
-        setClient(e.target.value)
+        setClient(e.target.checked)
     }
     function handleChangeOrder(e) {
         e.preventDefault()
-        setOrder(e.target.value)
+        setOrder(e.target.checked)
     }
     function handleChangeProvider(e) {
         e.preventDefault()
-        setProvider(e.target.value)
+        setProvider(e.target.checked)
     }
     function handleChangeHeigth(e) {
         e.preventDefault()
@@ -58,11 +62,25 @@ function TempleteForm() {
 
     const navigate = useNavigate();
     
+    
     function postPlantilla(e) {
         e.preventDefault()
         const ls = new LabelService();
+        var newDescription = false
+        var newBarCode = false
+        var newDate = false
+        var newClient = false
+        var newOrder = false
+        var newProvider = false
 
-        const plantillaACrear = {nombre : name, descripcion: description, fecha: date, ordenCompra: order, proveedor: provider, codigoBarras: barCode, cliente: client, alto: heigth, ancho: width};
+        description === true ? newDescription = true : newDescription = false
+        barCode === true ? newBarCode = true : newBarCode = false
+        date === true ? newDate = true : newDate = false
+        client === true ? newClient = true : newClient = false
+        order === true ? newOrder = true : newOrder = false
+        provider === true ? newProvider = true : newProvider = false
+
+        const plantillaACrear = {nombre : name, descripcion: newDescription, fecha: newDate, ordenCompra: newOrder, proveedor: newProvider, codigoBarras: newBarCode, cliente: newClient, alto: heigth, ancho: width};
 
         const JSONPlantilla = JSON.stringify(plantillaACrear)
 
@@ -76,7 +94,7 @@ function TempleteForm() {
     }
 
     return (
-        <Form>
+        <Form >
             <Row className="mb-3">
                 <Form.Group className="mb-3" controlId="text" >
                     <Form.Label>Nombre de plantilla</Form.Label>
@@ -86,29 +104,29 @@ function TempleteForm() {
                 <Form.Label>Palomea los campos que quieras para la plantilla</Form.Label>
 
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Descripción" value={description} onChange={handleChangeDescription}/>
+                    <Form.Check type="checkbox" label="Descripción" checked={!!description}  onChange={handleChangeDescription}/>
                 </Form.Group>
 
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Código de barras" value={barCode} onChange={handleChangeBarCode}/>
+                    <Form.Check type="checkbox" label="Código de barras" checked={barCode} onChange={handleChangeBarCode}/>
                 </Form.Group>
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Fecha" value={date} onChange={handleChangeDate}/>
+                    <Form.Check type="checkbox" label="Fecha" checked={date} onChange={handleChangeDate}/>
                 </Form.Group>
 
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Cliente" value={client} onChange={handleChangeClient}/>
+                    <Form.Check type="checkbox" label="Cliente" checked={client} onChange={handleChangeClient}/>
                 </Form.Group>
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Orden de compra" value={order} onChange={handleChangeOrder}/>
+                    <Form.Check type="checkbox" label="Orden de compra" checked={order} onChange={handleChangeOrder}/>
                 </Form.Group>
 
                 <Form.Group as={Col} className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Proveedor" value={provider} onChange={handleChangeProvider}/>
+                    <Form.Check type="checkbox" label="Proveedor" checked={provider} onChange={handleChangeProvider}/>
                 </Form.Group>
             </Row>
             <Row className="mb-3">
